79131445a5a2e697f4d3ea9540236019e501093e
drewdorris <contact@dorrisd.com>
2023-02-04 23:22:26 -0500

    fix check for cities not added, add more comments, remove warnings

--- cityioscript.py
+++ cityioscript.py
@@ -1,6 +1,7 @@
 import webbrowser
 import codecs
 import time
+import warnings
 from urllib.parse import unquote
 from seleniumwire import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -12,7 +13,11 @@ States = ["District of Columbia", "New Hampshire", "New Jersey", "New Mexico", "
 Cities = []
 CitiesNotAdded = []
 
+## I use a lot of selenium find element methods here, which is bad, but who wants these warnings printed every time
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
 ## open browser
+print("Opening browser")
 driver = webdriver.Firefox()
 driver.get(url)
 
@@ -20,13 +25,14 @@ driver.get(url)
 cityInput = driver.find_element_by_id("city-input")
 stateInput = Select(driver.find_element_by_id("state-input"))
 
-## open provided list
+## open provided text file
 with codecs.open(txtfile, encoding='utf-8') as f:
     for line in f:
         Cities.append(line)
 Cities.reverse()
 
-## take the list provided and strip away population info
+## loop through the provided list, strip info to just city/state, and enter into site
+print("Entering data")
 index = -1
 for line in Cities:
     index += 1
@@ -52,23 +58,34 @@ for line in Cities:
 ###########
 ## This section is for verifying all cities entered went through correctly.
 ## It could easily break in the future so if this breaks you can just remove this whole section
-for request in driver.requests:
-    if request.response:
-        if "cityquiz.io/api/cities" in request.url:
-            city = unquote(request.url[(request.url.index("&query=") + 7):request.url.index("&dropdown=")])
-            state = unquote(request.url[(request.url.index("&dropdown=") + 10):])
-            combined = city + " " + state
-            if combined not in Cities:
-                CitiesNotAdded.append(combined)
-            if "\"cities\": []" in str(request.response.body):
-                CitiesNotAdded.append(combined)
 
-##dead code for checking if the list of cities increased after entering a city
-##existingCities = driver.find_elements_by_xpath("//*[@class='my-cities-list']")
-##print(existingCities[0].get_attribute('start'))
+## This is the terrible solution for opening the list of all entered cities
+if len(Cities) > 20:
+    print("Waiting to check for any missed cities...")
+    ## jankiest solution: click the "show all" button to get the list of entered cities, then compare it with list in code
+    ## opening this can take some time for large lists... I don't know if I can somehow wait right until I know it's open
+    ## so I'll just wait 20 seconds and then hope it has loaded by then 
+    unopenedListenedCities = driver.find_elements_by_xpath("//*[@class='my-cities-list']")[0]
+    div = unopenedListenedCities.find_element_by_xpath("..")
+    button = div.find_element_by_xpath('.//button[@class="align-center"]').click()
+    time.sleep(20)
+
+## Get the list of cities now that the entire list is displayed
+openedListenedCities = driver.find_elements_by_xpath("//*[@class='my-cities-list']")[0]
+divAgain = openedListenedCities.find_element_by_xpath("..").text
+## Put it into a list and make it look like the one made from the text file above
+citiesList = divAgain.split('\n')
+citiesList = citiesList[1:len(citiesList) - 1]
+for index in range(len(citiesList)):
+    citiesList[index] = citiesList[index][0:citiesList[index].rindex('(') - 1]
+
+## Check if the entered cities actually made it in the list
+for cityEntered in Cities:
+    if cityEntered not in citiesList:
+        CitiesNotAdded.append(cityEntered)
 
 print(str(len(CitiesNotAdded)) + " cities missed!")
 for notAdded in CitiesNotAdded:
     print(notAdded)
-## End of section
+## End of verification section
 ###########
